29483c3f35c04860677fa024fe6f3b4c43b47ab0
UP210042 <up210042@alumnos.upa.edu.mx>
2024-04-08 20:02:04 -0600

    ey

---
+++ count-clicks/app.js
@@ -0,0 +1,18 @@
+let x=1
+let y=10
+let c=x-y
+//alert(c)
+//string
+let name="crscse";
+let name_1='aunsdiuasnu';
+let name_2=`sacacds`;
+//boolean
+let a=true && true ||false;
+let b=!false;
+let min_number=1<0;
+
+
+let button = document.getElementById("btn");//document -->> todo el documento html
+window.document.
+
+console.log(button);
